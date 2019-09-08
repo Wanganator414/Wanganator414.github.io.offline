1367bf6bf545965fd656c782953bc025827492a0
Wanganator <ewang414@gmail.com>
2019-09-08 12:41:43 +0800

    Updates

---
+++ precache-manifest.36bfb14c1ac73f724eb7c532c3d7bf0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb3f9aab8f9f9713c262651c9c3f85b",
+    "url": "https//wanganator414.github.io/index.html"
+  },
+  {
+    "revision": "dfd64a7d988dd6fe64db",
+    "url": "https//wanganator414.github.io/static/css/main.cbd4eff6.chunk.css"
+  },
+  {
+    "revision": "aeb552bfab6056933341",
+    "url": "https//wanganator414.github.io/static/js/2.64dd4d6d.chunk.js"
+  },
+  {
+    "revision": "dfd64a7d988dd6fe64db",
+    "url": "https//wanganator414.github.io/static/js/main.ee9c3753.chunk.js"
+  },
+  {
+    "revision": "90070814d69f962deb72",
+    "url": "https//wanganator414.github.io/static/js/runtime~main.731ac3b4.js"
+  }
+]);
--- src/Components/WOD/WodData.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from "react";
-
-function WodData() {
-  function apiCall() {}
-  return <div><p>HELLO</p></div>;
-}
-
-export default WodData;
